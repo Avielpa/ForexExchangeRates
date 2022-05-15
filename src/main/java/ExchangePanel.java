1c8eddb2e7dd7d0137baa840e9a856586800472c
ori mamo <mori8056@gmail.com>
2022-05-15 19:14:32 +0300

    update

--- src/main/java/ExchangePanel.java
+++ src/main/java/ExchangePanel.java
@@ -10,7 +10,8 @@ public class ExchangePanel extends JPanel {
 
     private ImageIcon background;
     private Document document;
-    private JLabel coinValueLabel, afterCalculate;
+    private JLabel coinValueLabel, afterCalculate,coinValueText;;
+    private JTextField amountOfMoney;
     private boolean isRunning = false;
 
     public ExchangePanel(int x,int y,int width,int hieght){
@@ -22,47 +23,62 @@ public class ExchangePanel extends JPanel {
         int xButton=10;
         int yButton=10;
         int xButtonReset=650;
-        int yButtonReset=300;
+        int yButtonReset=500;
         int xButtonCulculate=200;
         int yButtonCulculate=400;
         int dx=120;
+        int regulatWidth=100;
+        int regularHeight=50;
+        int culculateWidth=200;
+        int rculculateHeight=50;
 
-        JButton usdNis=createBotton(xButton,yButton,"usd-nis");
-        this.add(usdNis);
+
+        JButton usdNis=createBotton(xButton,yButton,"usd-nis",regulatWidth,regularHeight);
+        JButton euroNis=createBotton(xButton+dx,yButton,"euro-nis",regulatWidth,regularHeight);
+        JButton euroDollar=createBotton(xButton+2*dx,yButton,"euro-dollar",regulatWidth,regularHeight);
+        JButton euroPound=createBotton(xButton+3*dx,yButton,"euro-pound",regulatWidth,regularHeight);
+        JButton poundNis=createBotton(xButton+4*dx,yButton,"pound-nis",regulatWidth,regularHeight);
+        JButton reset=createBotton(xButtonReset,yButtonReset,"reset",regulatWidth,regularHeight);
+        JButton culculateExchange = createBotton(xButtonCulculate,yButtonCulculate,"culculate_exchange",culculateWidth,rculculateHeight);
         coinValueLabel =new JLabel();
         afterCalculate =new JLabel();
-        coinValueLabel.setBounds(100,300,100,50);
+        coinValueText =new JLabel();
+        amountOfMoney = new JTextField();
+
+
+        this.add(usdNis);
         this.add(coinValueLabel);
-        coinValueLabel.setVisible(false);
-        JTextField usernameTextField = new JTextField();
-        usernameTextField.setBounds(200,300,100,50);
-        this.add(usernameTextField);
-        usernameTextField.setVisible(false);
-        afterCalculate.setBounds(200,450,100,50);
-        this.add(afterCalculate);
-        coinValueLabel.setVisible(false);
-        JButton euroNis=createBotton(xButton+dx,yButton,"euro-nis");
+        this.add(coinValueText);
+        this.add(amountOfMoney);
         this.add(euroNis);
-        JButton euroDollar=createBotton(xButton+2*dx,yButton,"euro-dollar");
         this.add(euroDollar);
-        JButton euroPound=createBotton(xButton+3*dx,yButton,"euro-pound");
         this.add(euroPound);
-        JButton poundNis=createBotton(xButton+4*dx,yButton,"pound-nis");
         this.add(poundNis);
-        JButton reset=createBotton(xButtonReset,yButtonReset,"reset");
-        reset.setBackground(Color.CYAN);
+        this.add(afterCalculate);
         this.add(reset);
-        reset.setVisible(false);
-        JButton culculateExchange = createBotton(xButtonCulculate,yButtonCulculate,"culculate_exchange");
-        culculateExchange.setBackground(Color.CYAN);
         this.add(culculateExchange);
+
+        coinValueText.setBounds(100,300,150,50);
+        coinValueLabel.setBounds(250,300,50,50);
+        afterCalculate.setBounds(200,450,100,50);
+        amountOfMoney.setBounds(culculateExchange.getX()+culculateExchange.getWidth(),yButtonCulculate,100,50);
+        coinValueLabel.setVisible(false);
+        amountOfMoney.setVisible(false);
+        coinValueLabel.setVisible(false);
+        reset.setVisible(false);
         culculateExchange.setVisible(false);
+
+        reset.setBackground(Color.CYAN);
+        culculateExchange.setBackground(Color.CYAN);
+
+
 
         usdNis.addActionListener( (event)->{
                 setRunning(true);
                 run("pair_63","pid-63-bid");
                 coinValueLabel.setVisible(true);
-                usernameTextField.setVisible(true);
+                coinValueText.setVisible(true);
+                amountOfMoney.setVisible(true);
                 euroNis.setVisible(false);
                 euroDollar.setVisible(false);
                 euroPound.setVisible(false);
@@ -75,7 +91,8 @@ public class ExchangePanel extends JPanel {
             setRunning(true);
             run("pair_64","pid-64-bid");
             coinValueLabel.setVisible(true);
-            usernameTextField.setVisible(true);
+            coinValueText.setVisible(true);
+            amountOfMoney.setVisible(true);
             usdNis.setVisible(false);
             euroDollar.setVisible(false);
             euroPound.setVisible(false);
@@ -88,7 +105,8 @@ public class ExchangePanel extends JPanel {
             setRunning(true);
             run("pair_1","pid-1-bid");
             coinValueLabel.setVisible(true);
-            usernameTextField.setVisible(true);
+            coinValueText.setVisible(true);
+            amountOfMoney.setVisible(true);
             usdNis.setVisible(false);
             euroNis.setVisible(false);
             euroPound.setVisible(false);
@@ -100,7 +118,8 @@ public class ExchangePanel extends JPanel {
             setRunning(true);
             run("pair_6","pid-6-bid");
             coinValueLabel.setVisible(true);
-            usernameTextField.setVisible(true);
+            coinValueText.setVisible(true);
+            amountOfMoney.setVisible(true);
             usdNis.setVisible(false);
             euroNis.setVisible(false);
             euroDollar.setVisible(false);
@@ -112,7 +131,8 @@ public class ExchangePanel extends JPanel {
             setRunning(true);
             run("pair_65","pid-65-bid");
             coinValueLabel.setVisible(true);
-            usernameTextField.setVisible(true);
+            coinValueText.setVisible(true);
+            amountOfMoney.setVisible(true);
             usdNis.setVisible(false);
             euroNis.setVisible(false);
             euroDollar.setVisible(false);
@@ -129,9 +149,10 @@ public class ExchangePanel extends JPanel {
             reset.setVisible(false);
             culculateExchange.setVisible(false);
             coinValueLabel.setVisible(false);
+            coinValueText.setVisible(false);
             afterCalculate.setVisible(false);
-            usernameTextField.setVisible(false);
-            usernameTextField.setText("");
+            amountOfMoney.setVisible(false);
+            amountOfMoney.setText("");
             coinValueLabel.setText("");
             afterCalculate.setText("");
             setRunning(false);
@@ -139,16 +160,11 @@ public class ExchangePanel extends JPanel {
 
         });
         culculateExchange.addActionListener((event)->{
-            afterCalculate.setText("" + (Double.parseDouble(usernameTextField.getText()) * Double.parseDouble(coinValueLabel.getText())) +"");
+            afterCalculate.setText("" + (Double.parseDouble(amountOfMoney.getText()) * Double.parseDouble(coinValueLabel.getText())) +"");
             afterCalculate.setVisible(true);
             afterCalculate.setBackground(Color.WHITE);
             afterCalculate.setOpaque(true);
         });
-
-
-
-
-
     }
 
     public boolean isRunning() {
@@ -164,24 +180,18 @@ public class ExchangePanel extends JPanel {
         this.background.paintIcon(this,graphics,0,0);
     }
 
-    public JButton createBotton(int xButton ,int yButton,String name){
+    public JButton createBotton(int xButton ,int yButton,String name,int width,int height){
         JButton jButton = new JButton(name);
         jButton.setBackground(Color.WHITE);
-        jButton.setBounds(xButton, yButton, 100, 50);
+        jButton.setBounds(xButton, yButton, width, height);
         return jButton;
     }
-//    public JLabel createJlable(int yLabal,String name){
-//        JLabel jLabel=new JLabel(name);
-//        jLabel.setBackground(Color.CYAN);
-//        jLabel.setBounds(400,yLabal,150,50);
-//        return jLabel;
-//    }
 
     public void run(String s1,String s2){
         new Thread(()->{
             this.setFocusable(true);
             this.requestFocus();
-            while (isRunning){
+            while (isRunning){ //
                 try {
                     this.document= Jsoup.connect("https://il.investing.com/currencies/streaming-forex-rates-majors").get();
                     Element coinValue =this.document.getElementById("" + s1 + "");
@@ -189,6 +199,9 @@ public class ExchangePanel extends JPanel {
                     coinValueLabel.setText("" + coin.get(0).text() + "");
                     coinValueLabel.setBackground(Color.WHITE);
                     coinValueLabel.setOpaque(true);
+                    coinValueText.setText("The Current Value Is:");
+                    coinValueText.setBackground(Color.WHITE);
+                    coinValueText.setOpaque(true);
                     Thread.sleep(1);
                 } catch (Exception e) {
                     e.printStackTrace();
